8b01bce9cc5298e9618f07867f2b12410a3141c6
MityaAndreevich <dmitry.logachev@icloud.com>
2021-09-12 15:14:42 +0300

    in DataManager made a singletons patterned class Person, in model added an array of shuffled unique indexes of each array

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -16,22 +16,34 @@ struct Person {
     var fullName: String {
         "\(name) \(surname)"
     }
-    
-    static func getPersonList() -> [Person] {
-        [
-            Person(
-                name: "John",
-                surname: "Travolta",
-                phoneNumber: "88888",
-                email: "rt@ya"
-            ),
-            Person(
-                name: "Bill",
-                surname: "Tracy",
-                phoneNumber: "77777",
-                email: "mail"
+}
+extension Person {
+    static func getPersonsList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataManager.shared.names.shuffled()
+        let surnames = DataManager.shared.surnames.shuffled()
+        let phoneNumbers = DataManager.shared.phoneNumbers.shuffled()
+        let emails = DataManager.shared.emails.shuffled()
+        
+        let iterationCount = min(
+            names.count, surnames.count,
+            phoneNumbers.count, emails.count
+        )
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                surname: surnames[index],
+                phoneNumber: phoneNumbers[index],
+                email: emails[index]
             )
-        ]
+            
+            persons.append(person)
+        }
+        
+        return persons
     }
 }
 
--- ContactList/Services/DataManager.swift
+++ ContactList/Services/DataManager.swift
@@ -5,9 +5,37 @@
 //  Created by Дмитрий Логачёв on 09.09.2021.
 //
 
-import Foundation
-
-var names = ["John", "Tracy", "Bill", "Allen", "Katy", "Megan", "Zack"]
-var surnames = ["Carpenter", "Singer", "Murray", "Von-Dries", "Holland", "Foxx", "Snider"]
-var phoneNumbers = ["111-11-11", "222-22-22", "333-33-33", "444-44-44", "555-55-55", "666-66-66", "777-77-77"]
-var emails = ["Funk@Rock", "Big@Boss", "Richy@Rich", "Swanky@Tunes", "Little@Pig", "Monster@Mommy", "Lazy@Ass"]
+class DataManager {
+    
+    static let shared = DataManager()
+    
+    let names = [
+        "John", "Tracy", "Bill",
+        "Allen", "Katy", "Megan",
+        "Zack", "Kurtis", "Bob",
+        "Cristiano"
+    ]
+    
+    let surnames = [
+        "Carpenter", "Singer", "Murray",
+        "Von-Dries", "Holland", "Foxx",
+        "Snider", "Ronaldo", "Thoronton",
+        "Jhosef"
+    ]
+    
+    let phoneNumbers = [
+        "111-11-11", "222-22-22", "333-33-33",
+        "444-44-44", "555-55-55", "666-66-66",
+        "777-77-77", "888-88-88", "999-99-99",
+        "000-00-00"
+    ]
+    
+    let emails = [
+        "Funk@Rock", "Big@Boss", "Richy@Rich",
+        "Swanky@Tunes", "Little@Pig", "Monster@Mommy",
+        "Lazy@Ass", "Cri@ro", "Tiny@Cup",
+        "Lucky@Eagle"
+    ]
+    
+    private init() {}
+}
