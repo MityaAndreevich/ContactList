87581cf39c10df3cb78856addb8a2e6c13f13291
MityaAndreevich <dmitry.logachev@icloud.com>
2021-09-12 21:22:28 +0300

    pictures were added to the whole details view

--- ContactList/ViewControllers/WholeInfoListViewController.swift
+++ ContactList/ViewControllers/WholeInfoListViewController.swift
@@ -36,9 +36,11 @@ class WholeInfoListViewController: UITableViewController {
         switch indexPath.row {
         case 0:
             content.text = person.phoneNumber
-            //content.image = UIImage(systemName: Contacts.phone.rawValue)
+            content.image = UIImage(systemName: "phone")
         default:
             content.text = person.email
+            content.image = UIImage(systemName: "tray")
+            
         }
         
         cell.contentConfiguration = content
@@ -46,50 +48,5 @@ class WholeInfoListViewController: UITableViewController {
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
