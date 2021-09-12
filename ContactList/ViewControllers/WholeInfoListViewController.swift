2fb6e9dd8ad9715ad43826c0e89aeddf0dd79c26
MityaAndreevich <dmitry.logachev@icloud.com>
2021-09-12 21:25:08 +0300

    possibility of deselecting rows in whole details VC added

--- ContactList/ViewControllers/WholeInfoListViewController.swift
+++ ContactList/ViewControllers/WholeInfoListViewController.swift
@@ -16,20 +16,20 @@ class WholeInfoListViewController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         wholeInfoList.count
     }
-
+    
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         wholeInfoList[section].fullName
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       2
+        2
     }
     
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
+        
         let person = wholeInfoList[indexPath.section]
         var content = cell.defaultContentConfiguration()
         
@@ -44,9 +44,11 @@ class WholeInfoListViewController: UITableViewController {
         }
         
         cell.contentConfiguration = content
-
+        
         return cell
     }
     
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
